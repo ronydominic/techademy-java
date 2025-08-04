9456aca5fc061d7a4358769c4565704685e3efae
ronydominic <ronydominic620@gmail.com>
2025-08-04 10:30:24 +0530

    Renamed

--- aug2/lab9.java
+++ aug2/lab9.java
@@ -1,12 +1,10 @@
 package aug2.pack1;
 
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
-public class lab9 {
+public class Lab9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
