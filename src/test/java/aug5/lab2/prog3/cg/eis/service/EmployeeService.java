95d5791ad6b7d63804cca3bee2c7639c3d002626
ronydominic <ronydominic620@gmail.com>
2025-08-07 19:46:58 +0530

    lab2 prog3 1st push

---
+++ src/test/java/aug5/lab2/prog3/cg/eis/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package aug5.lab2.prog3.cg.eis.service;
+
+import aug5.lab2.prog3.cg.eis.bean.Employee;
+
+public interface EmployeeService {
+    public String determineInsuranceScheme(Employee employee);
+    public void displayDetails(Employee employee);
+
+
+}
